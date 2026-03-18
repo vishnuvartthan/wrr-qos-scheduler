a499fa4be2bdcde5f1a493b72f0e0a444c7e0bbd
V <vishnuvartthan@icloud.com>
2026-03-18 06:07:03 -0400

    Update synth.tcl

--- scripts/synth.tcl
+++ scripts/synth.tcl
@@ -1,13 +1,13 @@
 
-set_db init_lib_search_path ref/models
+set_db init_lib_search_path ./ref/models
 set_db library saed32hvt_tt1p05v25c.lib
 
 set_db init_hdl_search_path {. ./rtl }
 
-read_hdl {scheduler_pkg.sv req_fifo.sv age_tracker.sv wrr_arbiter.sv scheduler_core.sv csr_regs.sv status_counters.sv scheduler_top.sv }
+read_hdl -sv {scheduler_pkg.sv req_fifo.sv age_tracker.sv wrr_arbiter.sv scheduler_core.sv csr_regs.sv status_counters.sv scheduler_top.sv }
 
 elaborate scheduler_top
-read_sdc .scripts/constraints.sdc
+read_sdc ./scripts/constraints.sdc
 
 syn_generic
 syn_map
