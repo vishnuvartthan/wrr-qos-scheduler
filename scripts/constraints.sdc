99c469f24a875fd636e4b224efe4494ce332eeb1
V <vishnuvartthan@icloud.com>
2026-03-18 18:18:20 -0400

    Synth Update

--- scripts/constraints.sdc
+++ scripts/constraints.sdc
@@ -1,7 +1,9 @@
 
 create_clock -name clk -period 10 [get_ports clk]
 
-set_clock_uncertainty 0.2 [get_clocks clk]
+set_clock_transition -rise 0.1 [get_clocks "clk"]
+set_clock_transition -fall 0.1 [get_clocks "clk"]
+set_clock_uncertainty 0.01 [get_clocks clk]
 
-set_input_delay  1.0 -clock clk [remove_from_collection [all_inputs] [get_ports clk]]
-set_output_delay 1.0 -clock clk [all_outputs]
+set_input_delay  -max 1.0 -clock clk [remove_from_collection [all_inputs] [get_ports {clk rst_n}]]
+set_output_delay -max 1.0 -clock clk [all_outputs]
--- scripts/synth.tcl
+++ scripts/synth.tcl
@@ -1,26 +1,24 @@
+set_db init_lib_search_path <USER_LIB_PATH>
+set_db init_hdl_search_path <USER_HDL_PATH>
 
-set_db init_lib_search_path ./ref/models
-set_db library saed32hvt_tt1p05v25c.lib
-
-set_db init_hdl_search_path {. ./rtl }
+read_libs read_libs <USER_STD_CELL_LIB>
 
 read_hdl -sv {scheduler_pkg.sv req_fifo.sv age_tracker.sv wrr_arbiter.sv scheduler_core.sv csr_regs.sv status_counters.sv scheduler_top.sv }
 
 elaborate scheduler_top
-read_sdc ./scripts/constraints.sdc
+read_sdc ../scripts/constraints.sdc
 
 syn_generic
 syn_map
 syn_opt
 
-report_timing > synth/reports/timing.rpt
-report_timing -max_paths 10 > synth/reports/timing.rpt
-report_area > synth/reports/area.rpt
-report_power > synth/reports/power.rpt
-report_qor > synth/reports/qor.rpt
+report_timing > reports/timing.rpt
+report_area > reports/area.rpt
+report_power > reports/power.rpt
+report_qor > reports/qor.rpt
 
-write_hdl > synth/netlist/scheduler_top_syn.v
-write_sdc > synth/netlist/scheduler_top_syn.sdc
-
+write_hdl > outputs/scheduler_netlist.v
+write_sdc > outputs/scheduler_sdc.sdc
+write_sdf -timescale ns -nonegchecks -recrem split -edges check_edge -setuphold split > outputs/delays.sdf
 
 exit
